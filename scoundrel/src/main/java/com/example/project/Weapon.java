7bc9396dfd1cf9fba8af785da190f2a23fa3586a
tewenhao <enhaotew@gmail.com>
2026-06-30 20:14:10 +0100

    remembered that weapons need to maintain a list of monsters it slained

--- scoundrel/src/main/java/com/example/project/Weapon.java
+++ scoundrel/src/main/java/com/example/project/Weapon.java
@@ -1,29 +1,34 @@
 package com.example.project;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /*
  * The 9 Diamonds in the deck are Weapons. Red face cards are removed from the deck.
 */
 public class Weapon extends Card {
-    private int highestMonsterAttacked;
+    private int lowestMonsterAttacked;
+    private List<Monster> slainMonsters = new ArrayList<>(); // game requires weapon slain monsters to be stacked on top of weapon
 
     public Weapon (int val) {
         super(CardSuit.DIAMOND, val);
-        this.highestMonsterAttacked = 15;
+        this.lowestMonsterAttacked = 15;
     }
 
-    public int getHighestMonsterAttacked () {
+    public int getlowestMonsterAttacked () {
         // 15 - no monster attacked before
         // since 14 is the highest value (Ace)
-        return this.highestMonsterAttacked;
+        return this.lowestMonsterAttacked;
     }
 
     public void updateMonsterAttacked (Monster monster) throws MonsterValueTooHighException {
-        if (monster.getOrderedValue() >= this.highestMonsterAttacked) {
+        if (monster.getOrderedValue() >= this.lowestMonsterAttacked) {
             throw new MonsterValueTooHighException();
         }
 
         else {
-            this.highestMonsterAttacked = monster.getOrderedValue();
+            this.lowestMonsterAttacked = monster.getOrderedValue();
+            this.slainMonsters.add(monster);
         }
     }
 }
